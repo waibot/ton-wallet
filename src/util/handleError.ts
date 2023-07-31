4494e1c52662c3902ce575096dbd9ecf0d26bb29
Barry <wai1001@proton.me>
2023-07-31 18:12:46 +0800

    Update handleError.ts

--- src/util/handleError.ts
+++ src/util/handleError.ts
@@ -25,6 +25,6 @@ export function handleError(err: Error) {
   console.error(err);
 
   if (APP_ENV === 'development' || APP_ENV === 'staging') {
-    throttledAlert(`${DEBUG_ALERT_MSG}\n\n${(err?.message) || err}\n${err?.stack}`);
+    //throttledAlert(`${DEBUG_ALERT_MSG}\n\n${(err?.message) || err}\n${err?.stack}`);
   }
 }
